12599e09b4cc7645cb781a85869c133e1ee9aa50
Pavel Ivanov <pivanof@google.com>
2020-02-25 10:48:56 -0800

    Fix formatting of the code.
    PiperOrigin-RevId: 297143688
    PiperOrigin-RevId: 297152676
    Change-Id: I848fc8ef0ebe13f38fac4ddda0b29162fd15b65e

--- monitoring.cc
+++ monitoring.cc
@@ -16,46 +16,46 @@
 #include "absl/strings/string_view.h"
 
 namespace monitoring {
-  CallbackMetric<std::string>* master_connection_status;
-  CallbackMetric<std::string>* last_master_connect_error;
-  CallbackMetric<uint64_t>* time_since_master_last_connected;
-  Metric<bool>* rippled_active;
-  Metric<uint64_t>* bytes_sent_to_master;
-  Metric<uint64_t>* bytes_received_from_master;
+CallbackMetric<std::string>* master_connection_status;
+CallbackMetric<std::string>* last_master_connect_error;
+CallbackMetric<uint64_t>* time_since_master_last_connected;
+Metric<bool>* rippled_active;
+Metric<uint64_t>* bytes_sent_to_master;
+Metric<uint64_t>* bytes_received_from_master;
 
-  Metric<uint32_t, std::string>* slave_current_event_timestamp;
-  CallbackMetric<std::string, std::string>* slave_connection_status;
-  CallbackMetric<std::string, std::string>* last_slave_connect_error;
-  Metric<uint>* num_slaves;
-  Metric<uint64_t, std::string>* bytes_sent_to_slave;
-  Metric<uint64_t, std::string>* bytes_received_from_slave;
+Metric<uint32_t, std::string>* slave_current_event_timestamp;
+CallbackMetric<std::string, std::string>* slave_connection_status;
+CallbackMetric<std::string, std::string>* last_slave_connect_error;
+Metric<uint>* num_slaves;
+Metric<uint64_t, std::string>* bytes_sent_to_slave;
+Metric<uint64_t, std::string>* bytes_received_from_slave;
 
-  Counter<std::string>* rippled_binlog_error;
-  // Note about how this metric is used in binlog.cc: Most events are written
-  // to the Binlog via WriteEvent, and we set this metric's value there.
-  // However, FormatDescriptorEvents are written to the Binlog in custom
-  // functions that rebuild FormatDescriptors from fields and discard the event
-  // timestamps, so we must log these events from Binlog::AddEvent directly
-  // while their timestamps are still valid.
-  Metric<uint32_t>* binlog_last_event_timestamp;
-  Metric<uint64_t>* binlog_last_event_received;
+Counter<std::string>* rippled_binlog_error;
+// Note about how this metric is used in binlog.cc: Most events are written
+// to the Binlog via WriteEvent, and we set this metric's value there.
+// However, FormatDescriptorEvents are written to the Binlog in custom
+// functions that rebuild FormatDescriptors from fields and discard the event
+// timestamps, so we must log these events from Binlog::AddEvent directly
+// while their timestamps are still valid.
+Metric<uint32_t>* binlog_last_event_timestamp;
+Metric<uint64_t>* binlog_last_event_received;
 
-  void Initialize() {
-    bytes_sent_to_master = new Metric<uint64_t>();
-    bytes_received_from_master = new Metric<uint64_t>();
-    bytes_sent_to_slave = new Metric<uint64_t, std::string>();
-    bytes_received_from_slave = new Metric<uint64_t, std::string>();
-    master_connection_status = new CallbackMetric<std::string>();
-    last_master_connect_error = new CallbackMetric<std::string>();
-    time_since_master_last_connected = new CallbackMetric<uint64_t>();
-    rippled_active = new Metric<bool>();
-    slave_current_event_timestamp = new Metric<uint32_t, std::string>();
-    slave_connection_status = new CallbackMetric<std::string, std::string>();
-    last_slave_connect_error = new CallbackMetric<std::string, std::string>();
-    num_slaves = new Metric<uint>();
-    rippled_binlog_error = new Counter<std::string>();
-    binlog_last_event_timestamp = new Metric<uint32_t>();
-    binlog_last_event_received = new Metric<uint64_t>();
-  }
+void Initialize() {
+  bytes_sent_to_master = new Metric<uint64_t>();
+  bytes_received_from_master = new Metric<uint64_t>();
+  bytes_sent_to_slave = new Metric<uint64_t, std::string>();
+  bytes_received_from_slave = new Metric<uint64_t, std::string>();
+  master_connection_status = new CallbackMetric<std::string>();
+  last_master_connect_error = new CallbackMetric<std::string>();
+  time_since_master_last_connected = new CallbackMetric<uint64_t>();
+  rippled_active = new Metric<bool>();
+  slave_current_event_timestamp = new Metric<uint32_t, std::string>();
+  slave_connection_status = new CallbackMetric<std::string, std::string>();
+  last_slave_connect_error = new CallbackMetric<std::string, std::string>();
+  num_slaves = new Metric<uint>();
+  rippled_binlog_error = new Counter<std::string>();
+  binlog_last_event_timestamp = new Metric<uint32_t>();
+  binlog_last_event_received = new Metric<uint64_t>();
+}
 
 }   // namespace monitoring
